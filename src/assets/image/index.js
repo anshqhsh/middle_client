c94f716d7a772fc21342462a328a9ab4706c71b3
JoonHyuk Lee <60730765+anshqhsh@users.noreply.github.com>
2022-06-09 01:50:47 +0900

    chore: add image item

--- src/assets/image/index.js
+++ src/assets/image/index.js
@@ -1,7 +1,40 @@
-import aeron from './aeron.jpeg'
-import applewatch from './applewatch.jpeg'
-import audio from './audio.jpg'
-import dell from './dell.jpeg'
-import mbp16 from './mbp16.jpeg'
+import aeron from './itemImage/aeron.jpeg'
+import applewatch from './itemImage/applewatch.jpeg'
+import audio from './itemImage/audio.jpg'
+import dell from './itemImage/dell.jpeg'
+import mbp16 from './itemImage/mbp16.jpeg'
+import slider1 from './slideImage/1.jpg'
+import slider2 from './slideImage/2.jpg'
+import slider3 from './slideImage/3.jpg'
+import slider4 from './slideImage/4.jpg'
+import slider5 from './slideImage/5.jpg'
+import slider6 from './slideImage/6.jpg'
+import slider7 from './slideImage/7.jpg'
+import slider8 from './slideImage/8.jpg'
+import slider9 from './slideImage/9.jpg'
+import slider10 from './slideImage/10.jpg'
 
-export const ImgSrcs = [aeron, applewatch, audio, dell, mbp16]
+export const ImgSrcs = [
+  {
+    itemId: '허먼밀러 에어론',
+    src: aeron,
+  },
+  {
+    itemId: '벨킨 애플워치 스탠드',
+    src: applewatch,
+  },
+  {
+    itemId: '오디오엔진 a2',
+    src: audio,
+  },
+  {
+    itemId: 'dell 3219q',
+    src: dell,
+  },
+  {
+    itemId: '맥북프로 16',
+    src: mbp16,
+  },
+]
+
+export const SliderItem = [slider1, slider2, slider3, slider4, slider5, slider6, slider7, slider8, slider9, slider10]
--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,36 +1,13 @@
-import ProductItem from 'components/ProductItem'
+import { ImgSrcs } from 'assets/image'
+import ProductItem from 'components/ProductList/ProductItem'
 
 import styles from './productList.module.scss'
-
-const items = [
-  {
-    itemId: '허먼밀러 에어론',
-    src: 'https://lh4.googleusercontent.com/qgOdGeq82Esk-3IL28jD4tS_adglplvBe6QgfDZyJcoOCeLYvb2tTfB1uhOkevlYo91xKzQs5MSh6q6Ea1me=w2216-h2488',
-  },
-  {
-    itemId: '벨킨 애플워치 스탠드',
-    src: 'https://lh4.googleusercontent.com/fh12NsS5Hf5iqVjjF2vWG4PvA8iCG_WTMAoQqbosTVKqe3OY-AMnHod1f9o_wU0ILeN7bDBWowG6lliyI5Jn=w1078-h2374',
-  },
-  {
-    itemId: '맥북 16인치',
-    src: 'https://lh6.googleusercontent.com/RcdL73EDszZQB9gLVq8sgfX1Z37i25dO0_eH8JutY7Xogy2oSREhr7BjAN1z0oFpO56QWN3ZC-ZqA-oesWZ-=w1078-h2374-rw',
-  },
-
-  {
-    itemId: '오디오엔진 a2',
-    src: 'https://lh5.googleusercontent.com/qCpIPrc8aFpfiBi9I6Hj0X3HmpCQZXLFUXSfsAgW5PIFQsDNwRNzopzbCHRaM6vHujyavEqUKgKkYOJy6Zsy=w1078-h2374',
-  },
-  {
-    itemId: 'dell 3219q',
-    src: 'https://lh3.googleusercontent.com/CDjhoKKm9OikrY3WTrVfVCJtJtDG3SLvYs9c3O0Tt3MhkwijHv4NLix86dbOjZt6OX2EZr7B28WGC7c6G1Sd=w1078-h2374',
-  },
-]
 
 const ProductList = () => {
   return (
     <div className={styles.productContainer}>
-      {items.map((item) => (
-        <ProductItem key={item.itemId} itemId={item.itemId} src={item.src} />
+      {ImgSrcs.map((item) => (
+        <ProductItem key={2} itemId={item.itemId} src={item.src} />
       ))}
     </div>
   )
