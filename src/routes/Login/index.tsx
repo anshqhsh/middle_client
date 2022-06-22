89b6003dab6d31870d6bf34921f9b4267976b79a
JoonHyuk Lee <60730765+anshqhsh@users.noreply.github.com>
2022-06-23 00:42:50 +0900

    chore:login CSS

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -3,12 +3,30 @@ import styles from './login.module.scss'
 const Login = () => {
   return (
     <div className={styles.loginContainer}>
-      <h1>MIDDLE</h1>
+      <div className={styles.titleWrap}>
+        <h1>MIDDLE</h1>
+      </div>
       <form className={styles.loginForm}>
-        <input type='text' placeholder='id' />
-        <input type='text' placeholder='pw' />
+        <label>
+          <span>전화번호, 사용자 이름 또는 이메일</span>
+          <input type='text' />
+        </label>
+        <label>
+          <span>비밀 번호</span>
+          <input type='text' />
+        </label>
         <button type='submit'>로그인</button>
       </form>
+      <div className='gr27e'>
+        <div className=''>
+          <p className=''>
+            계정이 없으신가요?{' '}
+            <a>
+              <span className=''>가입하기</span>
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
